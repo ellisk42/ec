4feefc938607b31b395f5485e25ad1973a9988e8
Kevin Ellis <ellisk@mit.edu>
2018-01-15 20:47:23 -0500

    arithmetic primitives in separate file

---
+++ arithmeticPrimitives.py
@@ -0,0 +1,13 @@
+from program import *
+from type import *
+
+def _addition(x): return lambda y: x + y
+addition = Primitive("+",
+                     arrow(tint,arrow(tint,tint)),
+                     _addition)
+def _multiplication(x): return lambda y: x*y
+multiplication = Primitive("*",
+                           arrow(tint,arrow(tint,tint)),
+                           _multiplication)
+k1 = Primitive("1",tint,1)
+k0 = Primitive("0",tint,0)
