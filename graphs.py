5056013b8d65586db6acfe8fa8e7618f55dd77fe
Kevin Ellis <ellisk@mit.edu>
2018-01-18 14:24:00 -0500

    improved graphing

--- graphs.py
+++ graphs.py
@@ -3,34 +3,60 @@ from ec import *
 import matplotlib.pyplot as plot
 from matplotlib.ticker import MaxNLocator
 
-def plotECResult(results, colors = 'rgbky'):
+class Bunch(object):
+    def __init__(self,d):
+        self.__dict__.update(d)
+
+def parseResultsPath(p):
+    p = p[:p.rfind('.')]
+    domain = p[p.index('/')+1 : p.index('_')]
+    parameters = { k: eval(v)
+                   for binding in p.split('_')[1:]
+                   for [k,v] in [binding.split('=')] }
+    parameters['domain'] = domain
+    return Bunch(parameters)
+
+def plotECResult(results, colors = 'rgbky', label = None, title = None):
+    parameters = []
     for j,result in enumerate(results):
+        parameters.append(parseResultsPath(result))
         with open(result,'rb') as handle: results[j] = pickle.load(handle)
 
     f,a1 = plot.subplots()
+    a1.set_xlabel('Iteration')
+    a1.xaxis.set_major_locator(MaxNLocator(integer = True))
+    a1.set_ylabel('% Hit Tasks (solid)')
     a2 = a1.twinx()
-    for color, result in zip(colors, results):
-        a1.plot(range(1,len(result.learningCurve) + 1),
-                [ 100. * x / len(result.taskSolutions)
-                  for x in result.learningCurve],
-                color + '-')
-        a1.set_xlabel('Iteration')
-        a1.xaxis.set_major_locator(MaxNLocator(integer = True))
+    a2.set_ylabel('Avg description length in nats (dashed)')
 
-        a1.set_ylabel('% Hit Tasks')
+
+    for j, (color, result) in enumerate(zip(colors, results)):
+        l, = a1.plot(range(1,len(result.learningCurve) + 1),
+                    [ 100. * x / len(result.taskSolutions)
+                      for x in result.learningCurve],
+                    color + '-')
+        if label is not None:
+            l.set_label(label(parameters[j]))
 
         a2.plot(range(1,len(result.averageDescriptionLength) + 1),
                 result.averageDescriptionLength,
                 color + '--')
 
-        a2.set_ylabel('Average description length (nats)')
+    a1.set_ylim(ymin = 0)
+    a2.set_ylim(ymin = 0)
+    
+    if title is not None:
+        plot.title(title)
 
-
-
+    if label is not None:
+        a1.legend()
+        
     f.tight_layout()
     plot.show()
 
 
 if __name__ == "__main__":
     import sys
-    plotECResult(sys.argv[1:])
+    plotECResult(sys.argv[1:],
+                 title = "no neural network",
+                 label = lambda p: "%s %s"%(p.domain, p.frontierSize))
