8bf1f87a20baaaf3505aea98f0075c26095cfcd3
Kevin Ellis <ellisk@mit.edu>
2018-01-18 22:34:36 -0500

    added prime end square to list primitives

--- listPrimitives.py
+++ listPrimitives.py
@@ -1,5 +1,7 @@
 from program import *
 
+import math
+
 def _multiplication(x): return lambda y: x*y
 def _addition(x): return lambda y: x + y
 def _subtraction(x): return lambda y: x - y
@@ -15,6 +17,11 @@ def _mod(x): return lambda y: x%y
 def _not(x): return not x
 def _gt(x): return lambda y: x > y
 def _index(j): return lambda l: l[j]
+def _isPrime(n):
+    return n in [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199]
+def _isSquare(n):
+    r = int(math.sqrt(n))
+    return r*r == n
 primitives = [
     Primitive("index",arrow(tint,tlist(tint),tint),_index),
     Primitive("+",arrow(tint,tint),_addition),
@@ -32,5 +39,7 @@ primitives = [
     Primitive("eq?",arrow(tint,tint,tbool), _eq),
     Primitive("mod",arrow(tint,tint,tint), _mod),
     Primitive("not",arrow(tbool,tbool), _not),
-    Primitive("gt?",arrow(tint,tint,tbool), _gt)    
+    Primitive("gt?",arrow(tint,tint,tbool), _gt)    ,
+    Primitive("is-prime",arrow(tint,tbool),_isPrime),
+    Permitted("is-square",arrow(tint,tbool),_isSquare),
 ] + [ Primitive(str(j),tint,j) for j in range(10) ]
