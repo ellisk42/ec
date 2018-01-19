255a8d10d943e32bb0b3502d6eacf37d1f26a1b2
Kevin Ellis <ellisk@mit.edu>
2018-01-19 09:11:15 -0500

    re- factored text problems

--- makeTextTasks.py
+++ makeTextTasks.py
@@ -29,99 +29,92 @@ singleWordOperations = {"lowercase": lambda x: x.lower(),
 
 def makeTasks():
     NUMBEROFEXAMPLES = 4
-    singleWordProblems = [RegressionTask(n, arrow(tstring,tstring),
-                                         [((x,),f(x)) for _ in range(NUMBEROFEXAMPLES) for x in [randomWord()] ])
-                          for n,f in singleWordOperations.iteritems() ]
-    doubleWordProblems = [RegressionTask(n1 + "." + n2, arrow(tstring,tstring),
-                                         [((x,),f1(f2(x))) for _ in range(NUMBEROFEXAMPLES) for x in [randomWord()] ])
-                          for n1,f1 in singleWordOperations.iteritems()
-                          for n2,f2 in singleWordOperations.iteritems()
-                          if ("character" in n1) != ("character" in n2) and n1 < n2]
-    mapSingleProblems = [RegressionTask("Apply %s delimited by '%s' to input delimited by '%s'"%(n,d1,d2),
-                                        arrow(tstring,tstring),
-                                 [((x,),d2.join(map(f,x.split(d1))))
-                                  for _ in range(NUMBEROFEXAMPLES)
-                                  for x in [randomWords(d1)] ])
-                         for n,f in singleWordOperations.iteritems()
-                         for d1 in delimiters
-                         for d2 in delimiters
-                         if d1 != d2]
-    mapDoubleProblems = [RegressionTask("Apply %s.%s delimited by '%s' to input delimited by '%s'"%(n1,n2,d1,d2),
-                                        arrow(tstring,tstring),
-                                 [((x,),d2.join(map(f2,map(f1,x.split(d1)))))
-                                  for _ in range(NUMBEROFEXAMPLES)
-                                  for x in [randomWords(d1)] ])
-                         for n1,f1 in singleWordOperations.iteritems()
-                         for n2,f2 in singleWordOperations.iteritems()
-                         if ("character" in n1) != ("character" in n2) and n1 < n2
-                         for d1 in delimiters
-                         for d2 in delimiters ]
-    extractPrefix1 = [RegressionTask("Extract prefix up to '%s' (exclusive)"%d,
-                                     arrow(tstring,tstring),
-                                     [((x,),y)
+    problems = []
+    def problem(n, examples):
+        problems.append(RegressionTask(n, arrow(tstring,tstring),
+                                       [((x,),y) for x,y in examples ]))
+    
+    [problem(n, [(x,f(x)) for _ in range(NUMBEROFEXAMPLES) for x in [randomWord()] ])
+     for n,f in singleWordOperations.iteritems() ]
+    [problem(n1 + "." + n2, 
+             [(x,f1(f2(x))) for _ in range(NUMBEROFEXAMPLES) for x in [randomWord()] ])
+     for n1,f1 in singleWordOperations.iteritems()
+     for n2,f2 in singleWordOperations.iteritems()
+     if ("character" in n1) != ("character" in n2) and n1 < n2]
+    [problem("Apply %s delimited by '%s' to input delimited by '%s'"%(n,d1,d2),
+             [(x, d2.join(map(f,x.split(d1))))
+              for _ in range(NUMBEROFEXAMPLES)
+              for x in [randomWords(d1)] ])
+     for n,f in singleWordOperations.iteritems()
+     for d1 in delimiters
+     for d2 in delimiters
+     if d1 != d2]
+    [problem("Apply %s.%s delimited by '%s' to input delimited by '%s'"%(n1,n2,d1,d2),
+             [(x,d2.join(map(f2,map(f1,x.split(d1)))))
+              for _ in range(NUMBEROFEXAMPLES)
+              for x in [randomWords(d1)] ])
+     for n1,f1 in singleWordOperations.iteritems()
+     for n2,f2 in singleWordOperations.iteritems()
+     if ("character" in n1) != ("character" in n2) and n1 < n2
+     for d1 in delimiters
+     for d2 in delimiters ]
+    [problem("Extract prefix up to '%s' (exclusive)"%d,
+                                     [(x,y)
                                       for _ in range(NUMBEROFEXAMPLES)
                                       for y in [randomWord()]
                                       for x in [y + d + randomWord()]
                                      ])
-                      for d in delimiters ]
-    extractPrefix2 = [RegressionTask("Extract prefix up to '%s' (inclusive)"%d,
-                                     arrow(tstring,tstring),
-                                     [((x,),y)
+     for d in delimiters ]
+    [problem("Extract prefix up to '%s' (inclusive)"%d,
+                                     [(x,y)
                                       for _ in range(NUMBEROFEXAMPLES)
                                       for y in [randomWord() + d]
                                       for x in [y + d + randomWord()]
                                      ])
                       for d in delimiters ]
-    extractSuffix1 = [RegressionTask("Extract suffix up to '%s' (exclusive)"%d,
-                                     arrow(tstring,tstring),
-                                     [((x,),y)
+    [problem("Extract suffix up to '%s' (exclusive)"%d,
+                                     [(x,y)
                                       for _ in range(NUMBEROFEXAMPLES)
                                       for y in [randomWord()]
                                       for x in [randomWord() + d + y]
                                      ])
                       for d in delimiters ]
-    extractSuffix2 = [RegressionTask("Extract suffix up to '%s' (inclusive)"%d,
-                                     arrow(tstring,tstring),
-                                     [((x,),y)
+    [problem("Extract suffix up to '%s' (inclusive)"%d,
+                                     [(x,y)
                                       for _ in range(NUMBEROFEXAMPLES)
                                       for y in [randomWord() + d]
                                       for x in [randomWord() + d + y]
                                      ])
                       for d in delimiters ]
-    extractDelimited1 = [RegressionTask("Extract string delimited by '%s','%s'"%(d1,d2),
-                                        arrow(tstring,tstring),
-                                        [((x,),y)
+    [problem("Extract string delimited by '%s','%s'"%(d1,d2),
+                                        [(x,y)
                                  for _ in range(NUMBEROFEXAMPLES)
                                  for y in [randomWord()]
                                  for x in [randomWord() + d1 + y + d2 + randomWord()]])
                         for d1 in delimiters
                         for d2 in delimiters]
-    extractDelimited2 = [RegressionTask("Extract string delimited by '%s' (inclusive),'%s'"%(d1,d2),
-                                        arrow(tstring,tstring),
-                                [((x,),y)
+    [problem("Extract string delimited by '%s' (inclusive),'%s'"%(d1,d2),
+                                [(x,y)
                                  for _ in range(NUMBEROFEXAMPLES)
                                  for y in [d1 + randomWord() + d2]
                                  for x in [randomWord() + y + randomWord()]])
                         for d1 in delimiters
                         for d2 in delimiters]
-    extractDelimited3 = [RegressionTask("Extract string delimited by '%s' (inclusive),'%s' (inclusive)"%(d1,d2),
-                                        arrow(tstring,tstring),
-                                [((x,),y)
+    [problem("Extract string delimited by '%s' (inclusive),'%s' (inclusive)"%(d1,d2),
+                                [(x,y)
                                  for _ in range(NUMBEROFEXAMPLES)
                                  for y in [d1 + randomWord()]
                                  for x in [randomWord() + y + d2 + randomWord()]])
                         for d1 in delimiters
                         for d2 in delimiters] 
-    applyDelimited = [RegressionTask("Apply %s to string delimited by '%s','%s'"%(n,d1,d2),
-                                     arrow(tstring,tstring),
-                                [((x,),f(y))
+    [problem("Apply %s to string delimited by '%s','%s'"%(n,d1,d2),
+                                [(x,f(y))
                                  for _ in range(NUMBEROFEXAMPLES)
                                  for y in [randomWord()]
                                  for x in [randomWord() + d1 + y + d2 + randomWord()]])
                       for n,f in singleWordOperations.iteritems()
                         for d1 in delimiters
                         for d2 in delimiters]
-
-    return singleWordProblems + doubleWordProblems + extractPrefix1 + extractPrefix2 + extractSuffix1 + extractSuffix2 + mapSingleProblems + mapDoubleProblems + extractDelimited1 + extractDelimited2 + applyDelimited
+    return problems
 
 
