08bac39919a7f131d0edfd40bd16c0474301a0d3
Kevin Ellis <ellisk@mit.edu>
2018-01-27 11:41:39 -0500

    improvements to AWS script

--- launch.py
+++ launch.py
@@ -4,19 +4,22 @@ import os
 import json
 import sys
 
+
 def launch(size = "t2.micro"):
     # aws ec2 run-instances --image-id ami-835f6ae6 --instance-type "t2.micro" --key-name testing --associate-public-ip-address
     o = json.loads(subprocess.check_output(["aws","ec2","run-instances",
                                             "--image-id","ami-835f6ae6",
                                             "--instance-type",size,
                                             "--security-groups","publicssh",
+                                            "--instance-initiated-shutdown-behavior","terminate",
                                             "--key-name","testing"]))
-
+    print o
     instance = o[u"Instances"][0][u"InstanceId"]
     print "Launched instance", instance
     
     o = json.loads(subprocess.check_output(["aws","ec2","describe-instances",
                                             "--instance-ids",instance]))
+    print o
     address = o[u'Reservations'][0][u'Instances'][0][u'PublicIpAddress']
     print "Retrieved IP address of instance %s; got %s"%(instance,address)
     return instance, address
@@ -24,15 +27,36 @@ def launch(size = "t2.micro"):
 
 
 def sendCommand(address, script):
-    script = """
+    import tempfile
+    script = """#!/bin/bash
 cd ~/ec
 git pull
 mkdir jobs
-%s
-"""%script
-    command = "ssh -o StrictHostKeyChecking=no -i ~/.ssh/testing.pem ubuntu@%s \"%s\"" % (address, script)
-    os.system(command)
+""" + script
+    fd = tempfile.NamedTemporaryFile(mode = 'w',delete = False,dir = "/tmp")
+    fd.write(script)
+    fd.close()
+    name = fd.name
 
-instance, address = launch()
-time.sleep(120)
-sendCommand(address, "echo test > testOutput")
+    # Copy over the script
+    print "Copying script over to",address
+    os.system("scp -o StrictHostKeyChecking=no -i ~/.ssh/testing.pem %s ubuntu@%s:~/script.sh" % (name, address))
+
+    # delete local copy
+    os.system("rm %s"%name)
+    
+    # Execute the script
+    # For some reason you need to pipe the output to /dev/null in order to get it to detach
+    os.system("ssh -o StrictHostKeyChecking=no -i ~/.ssh/testing.pem ubuntu@%s 'bash ./script.sh > /dev/null 2>&1 &'" % (address))
+    print "Executing script on remote host."
+
+
+if __name__ == "__main__":
+    instance, address = launch()
+    time.sleep(60)
+    sendCommand(address, """
+    echo starting
+    echo test > testOutput
+    sleep 100
+    echo ending
+    """)
