cc84cc8a1b2fba2293d701657564f2dc19069a0b
Kevin Ellis <ellisk@mit.edu>
2018-02-11 17:54:10 -0500

    more verbosity

--- enumeration.py
+++ enumeration.py
@@ -72,7 +72,7 @@ def enumerateForTask(g, task, _ = None,
                 numberOfPrograms += 1
                 
                 likelihood = task.logLikelihood(p, timeout=evaluationTimeout)
-                if verbose and valid(likelihood):
+                if valid(likelihood):
                     eprint("Hit",task.name,"with the program",p,"which has prior",prior,"after",time() - starting,"seconds")
                     frontier.append(FrontierEntry(program = p,
                                                   logPrior = prior,
