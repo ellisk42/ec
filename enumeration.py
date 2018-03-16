46fd72a1f9e25c001ef052d84d5db5d3ae78fbeb
Kevin Ellis <ellisk@mit.edu>
2018-03-16 10:20:12 -0400

    check for whether there will be multiple threads working on a single task

--- enumeration.py
+++ enumeration.py
@@ -75,6 +75,10 @@ def multithreadedEnumeration(g, tasks, _=None,
     if not isinstance(g, dict): g = {t: g for t in tasks }
     task2grammar = g
 
+    allowMultipleThreadsPerTask = CPUs > len(tasks)
+    if allowMultipleThreadsPerTask:
+        eprint("EXPERIMENTAL: Multiple threads will be working on a single task simultaneously.")
+
     frontiers = {t: Frontier([], task = t) for t in task2grammar }
     activeTasks = set(task2grammar.keys())
     lowerBounds = {t: 0. for t in task2grammar}
@@ -124,7 +128,7 @@ def multithreadedEnumeration(g, tasks, _=None,
                     lowerBounds[t] += bi
                     workers[nextID] = t
                     nextID += 1
-                    
+
         if len(workers) > 0:
             ID, newFrontier, searchTime, explored = q.get()
             task = workers[ID]
