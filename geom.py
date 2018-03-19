d384c6e09f74cf4fa2a0f26b48208e477b7ee068
Artymort <m.berta@laposte.net>
2018-03-19 17:46:33 -0400

    Confirmed Conv2d net

--- geom.py
+++ geom.py
@@ -4,6 +4,7 @@ from utilities import eprint, testTrainSplit, numberOfCPUs
 from makeGeomTasks import makeTasks
 from geomPrimitives import primitives
 
+import torch
 import torch.nn as nn
 
 from recognition import variable
@@ -11,31 +12,32 @@ from recognition import variable
 
 # : Task -> feature list
 class GeomFeatureCNN(nn.Module):
-    def __init__(self, tasks, cuda=False, H=16):
+    def __init__(self, tasks, cuda=False, H=4):
         super(GeomFeatureCNN, self).__init__()
 
         size = 16  # Compute this maybe?
 
-        self.conv = nn.Conv1d(size, H, H)
+        self.conv = nn.Conv2d(size, H, 1)
         self.relu = nn.ReLU()
         self.out = nn.Sequential(self.conv, self.relu)
 
-        self.outputDimensionality = H
-        self.hidden = self.conv
+        self.outputDimensionality = size*H
+        self.hidden = self.out
 
     def featuresOfTask(self, t):  # Take a task and returns [features]
-        x = 16  # Should not hardocode these.
-        y = 16
+        x, y = 16, 16  # Should not hardocode these.
         onlyTask = t.examples[0][1]
         floatOnlyTask = map(float, onlyTask)
-        reshaped = [[floatOnlyTask[i:i+x]
-                    for i in range(0, len(floatOnlyTask), y)]]
+        reshaped = [floatOnlyTask[i:i+x]
+                    for i in range(0, len(floatOnlyTask), y)]
         variabled = variable(reshaped).float()
+        variabled = torch.unsqueeze(variabled, -1)
+        variabled = torch.unsqueeze(variabled, 0)
         x = self.out(variabled)
-        x = x.view(16)
-        return x.clamp(min=0)
+        x = torch.squeeze(x).view(4*16).clamp(min=0)
+        return x
 
-    def featuresOfProgram(self, p, t):  # Won't do for geom
+    def featuresOfProgram(self, p, t):  # Won't fix for geom
         return None
 
 
@@ -54,12 +56,12 @@ if __name__ == "__main__":
                            compressor="pypy",
                            evaluationTimeout=0.01,
                            **commandlineArguments(
-                               steps=5,
-                               iterations=5,
+                               steps=100000,
+                               iterations=10,
                                useRecognitionModel=True,
                                helmholtzRatio=0.0,
                                featureExtractor=GeomFeatureCNN,
                                topK=2,
-                               maximumFrontier=200,
+                               maximumFrontier=100,
                                CPUs=numberOfCPUs(),
                                pseudoCounts=10.0))
