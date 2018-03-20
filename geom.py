2f2a4223a1e76c03af35f6e64a3393bf3e21135d
Artymort <m.berta@laposte.net>
2018-03-19 23:22:11 -0400

    Toward LeNet-5

--- geom.py
+++ geom.py
@@ -12,17 +12,23 @@ from recognition import variable
 
 # : Task -> feature list
 class GeomFeatureCNN(nn.Module):
-    def __init__(self, tasks, cuda=False, H=4):
+    def __init__(self, tasks, cuda=False, H=10):
         super(GeomFeatureCNN, self).__init__()
 
-        size = 16  # Compute this maybe?
+        self.net1 = nn.Sequential(
+            nn.Conv2d(1, 6, kernel_size=(5, 5)),
+            nn.ReLU(),
+            nn.Conv2d(6, 16, kernel_size=(5, 5)),
+            nn.MaxPool2d(kernel_size=(2, 2), stride=2),
+            nn.ReLU()
+        )
+        self.net2 = nn.Sequential(
+            nn.Linear(256, 120),
+            nn.Linear(120, H)
+        )
 
-        self.conv = nn.Conv2d(size, H, 1)
-        self.relu = nn.ReLU()
-        self.out = nn.Sequential(self.conv, self.relu)
-
-        self.outputDimensionality = size*H
-        self.hidden = self.out
+        self.outputDimensionality = H
+        self.out = nn.Sequential(self.net1)
 
     def featuresOfTask(self, t):  # Take a task and returns [features]
         x, y = 16, 16  # Should not hardocode these.
@@ -31,11 +37,12 @@ class GeomFeatureCNN(nn.Module):
         reshaped = [floatOnlyTask[i:i+x]
                     for i in range(0, len(floatOnlyTask), y)]
         variabled = variable(reshaped).float()
-        variabled = torch.unsqueeze(variabled, -1)
         variabled = torch.unsqueeze(variabled, 0)
-        x = self.out(variabled)
-        x = torch.squeeze(x).view(4*16).clamp(min=0)
-        return x
+        variabled = torch.unsqueeze(variabled, 0)
+        output = self.net1(variabled)
+        output = output.view(256)
+        output = self.net2(output)
+        return output
 
     def featuresOfProgram(self, p, t):  # Won't fix for geom
         return None
@@ -54,14 +61,14 @@ if __name__ == "__main__":
                            testingTasks=test,
                            outputPrefix="experimentOutputs/geom",
                            compressor="pypy",
-                           evaluationTimeout=0.01,
+                           evaluationTimeout=0.1,
                            **commandlineArguments(
-                               steps=100000,
+                               steps=1000,
                                iterations=10,
-                               useRecognitionModel=True,
+                               useRecognitionModel=False,
                                helmholtzRatio=0.0,
                                featureExtractor=GeomFeatureCNN,
                                topK=2,
-                               maximumFrontier=100,
+                               maximumFrontier=1000,
                                CPUs=numberOfCPUs(),
                                pseudoCounts=10.0))
