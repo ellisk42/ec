4a5ad8221b3609fcb55e97056584b0cf478791e6
Kevin Ellis <ellisk@mit.edu>
2018-03-25 13:57:00 -0400

    git diff

--- makeListTasks.py
+++ makeListTasks.py
@@ -186,14 +186,14 @@ def make_list_bootstrap_tasks(numberOfExamples):
         Task("singleton", arrow(tint,tlist(tint)),
              [((n,),[n])
               for n in range(10) ]),
-        Task("length bool", arrow(tlist(tbool),tint),
-             [((l,), len(l))
-              for _ in range(10)
-              for l in [[random() > 0.5 for _ in range(randint(0,10)) ]] ]),
-        Task("length int", arrow(tlist(tint),tint),
-             [((l,), len(l))
-              for _ in range(10)
-              for l in [randomList()] ]),
+        # Task("length bool", arrow(tlist(tbool),tint),
+        #      [((l,), len(l))
+        #       for _ in range(10)
+        #       for l in [[random() > 0.5 for _ in range(randint(0,10)) ]] ]),
+        # Task("length int", arrow(tlist(tint),tint),
+        #      [((l,), len(l))
+        #       for _ in range(10)
+        #       for l in [randomList()] ]),
 
         # Task("range", arrow(tint,tlist(tint)),
         #      [((n,), range(1,1+n))
