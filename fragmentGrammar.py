1e80abea1eb65de6718c071d3735e56f4ed56a1d
Kevin Ellis <ellisk@mit.edu>
2018-04-04 19:50:48 -0400

    only rewriting frontiers once we have a lot of frontiers

--- fragmentGrammar.py
+++ fragmentGrammar.py
@@ -272,6 +272,8 @@ class FragmentGrammar(object):
                                frontiers)
         restrictedFrontiers = []
 
+        rewriteFrontiers = len(frontiers) > 20 # optimization hack
+
         def grammarScore(g):
             g = g.makeUniform().insideOutside(restrictedFrontiers, pseudoCounts)
             likelihood = g.jointFrontiersMDL(restrictedFrontiers)
@@ -323,13 +325,15 @@ class FragmentGrammar(object):
                        (newType,newPrimitive,newScore,dS,expectedUses))
                 
                 # Rewrite the frontiers in terms of the new fragment
-                concretePrimitive = defragment(newPrimitive)
-                bestGrammar.productions[-1] = (newPrimitiveLikelihood,
-                                               concretePrimitive.tp,
-                                               concretePrimitive)
-                frontiers = parallelMap(CPUs,
-                                        lambda frontier: bestGrammar.rescoreFrontier(RewriteFragments.rewriteFrontier(frontier, newPrimitive)),
-                                        frontiers)
+                if rewriteFrontiers:
+                    concretePrimitive = defragment(newPrimitive)
+                    bestGrammar.productions[-1] = (newPrimitiveLikelihood,
+                                                   concretePrimitive.tp,
+                                                   concretePrimitive)
+                    frontiers = parallelMap(CPUs,
+                                            lambda frontier: bestGrammar.rescoreFrontier(RewriteFragments.rewriteFrontier(frontier, newPrimitive)),
+                                            frontiers)
+                else: concretePrimitive = newPrimitive
                 eprint("\t(<uses> in rewritten frontiers: %f)"%
                        (bestGrammar.expectedUses(frontiers).actualUses[concretePrimitive]))
         else:
