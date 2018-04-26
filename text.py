c909030a135ccb4f70217d9aa58dc91b4e5bcab1
Kevin Ellis <ellisk@mit.edu>
2018-04-26 12:42:39 -0400

    text challenge problems

--- text.py
+++ text.py
@@ -1,7 +1,7 @@
 from ec import explorationCompression, commandlineArguments, Task
 from grammar import Grammar
 from utilities import eprint, testTrainSplit, numberOfCPUs
-from makeTextTasks import makeTasks, delimiters
+from makeTextTasks import makeTasks, delimiters, loadPBETasks
 from textPrimitives import primitives
 from listPrimitives import bootstrapTarget
 from program import *
@@ -32,12 +32,19 @@ if __name__ == "__main__":
     test, train = testTrainSplit(tasks, 0.9)
     eprint("Split tasks into %d/%d test/train"%(len(test),len(train)))
 
+    challenge, challengeCheating = loadPBETasks()
+    eprint("Got %d challenge PBE tasks"%len(challenge))
+
     baseGrammar = Grammar.uniform(primitives + bootstrapTarget())
+
+    evaluationTimeout = 0.0005
+    # We will spend five minutes on each challenge problem
+    challengeTimeout = 5 * 60
     
-    explorationCompression(baseGrammar, train,
+    generator = ecIterator(baseGrammar, train,
                            testingTasks = test,
                            outputPrefix = "experimentOutputs/text",
-                           evaluationTimeout = 0.0005,
+                           evaluationTimeout = evaluationTimeout,
                            compressor="pypy", # 
                            **commandlineArguments(
                                steps = 500,
@@ -47,7 +54,19 @@ if __name__ == "__main__":
                                maximumFrontier = 2,
                                structurePenalty = 10.,
                                a = 3,
-                               activation = "relu",
+                               activation = "tanh",
                                CPUs = numberOfCPUs(),
                                featureExtractor = LearnedFeatureExtractor,
                                pseudoCounts = 10.0))
+
+    for result in generator:
+        eprint("Evaluating on challenge problems...")
+        recognizer = result.recognitionModel
+        challengeFrontiers, _ = recognizer.enumerateFrontiers(challenge, likelihoodModel,
+                                                              CPUs=numberOfCPUs(),
+                                                              solver="ocaml",
+                                                              maximumFrontier=1,
+                                                              enumerationTimeout=300,
+                                                              evaluationTimeout=evaluationTimeout)
+        eprint("Challenge problem enumeration results:")
+        eprint(Frontier.describe(challengeFrontiers))
