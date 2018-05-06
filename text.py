45d565b1846425205ac24e007497cf02ed7d3c3d
Kevin Ellis <ellisk@mit.edu>
2018-05-06 10:18:07 -0400

    text recognition model now compatible with STRING

--- text.py
+++ text.py
@@ -10,6 +10,21 @@ from enumeration import *
 
 import random
 
+class ConstantInstantiateVisitor(object):
+    def __init__(self, words):
+        self.words = words
+    def primitive(self,e):
+        if e.name == "STRING": return Primitive("STRING", e.tp, random.choice(self.words))
+        return e
+    def invented(self,e): return e.body.visit(self)
+    def index(self,e): return e
+    def application(self,e):
+        return Application(e.f.visit(self), e.x.visit(self))
+    def abstraction(self,e):
+        return Abstraction(e.body.visit(self))
+
+
+
 class LearnedFeatureExtractor(RecurrentFeatureExtractor):
     def tokenize(self, examples):
         return examples
@@ -24,6 +39,10 @@ class LearnedFeatureExtractor(RecurrentFeatureExtractor):
                                                       H = 64,
                                                       tasks = tasks,
                                                       bidirectional = True)
+    def featuresOfProgram(self, p, tp):
+        # Instantiate STRING w/ random words
+        p = p.visit(ConstantInstantiateVisitor.SINGLE)
+        return super(LearnedFeatureExtractor, self).featuresOfProgram(p,tp)
 
 
 if __name__ == "__main__":
@@ -35,8 +54,12 @@ if __name__ == "__main__":
 
     challenge, challengeCheating = loadPBETasks()
     eprint("Got %d challenge PBE tasks"%len(challenge))
-    
 
+    ConstantInstantiateVisitor.SINGLE = \
+     ConstantInstantiateVisitor(map(list,list({ tuple([c for c in s])
+                                                for t in test + train + challenge
+                                                for s in t.stringConstants })))
+    
     baseGrammar = Grammar.uniform(primitives + bootstrapTarget())
 
     evaluationTimeout = 0.0005
