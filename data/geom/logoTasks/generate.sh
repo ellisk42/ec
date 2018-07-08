03b8aa87a50f027b2248095b4c700963318abad9
Artymort <m.berta@laposte.net>
2018-07-08 16:01:44 -0400

    LOGO Flowers

--- data/geom/logoTasks/generate.sh
+++ data/geom/logoTasks/generate.sh
@@ -15,3 +15,15 @@ for i in "5" "7"; do
   ../logoDrawString $resl "star_${i}_l" 0 "(lambda (logo_forLoopM ($i) (lambda (logo_FWRT (logo_MULL logo_UL 3) (logo_SUBA (logo_UA) (logo_DIVA (logo_MULA logo_UA 1) $i)) \$1)) \$0))"
   ../logoDrawString $resh "star_${i}_h" 0 "(lambda (logo_forLoopM ($i) (lambda (logo_FWRT (logo_MULL logo_UL 3) (logo_SUBA (logo_UA) (logo_DIVA (logo_MULA logo_UA 1) $i)) \$1)) \$0))"
 done
+
+../logoDrawString $resh "iter1_h" 0 "(lambda (logo_forLoopM logo_IFTY (lambda (logo_FWRT logo_epsL (logo_DIVA logo_epsA 2) \$1)) \$0))"
+../logoDrawString $resh "iter2_h" 0 "(lambda (logo_forLoopM 2 (lambda (logo_forLoopM logo_IFTY (lambda (logo_FWRT logo_epsL (logo_DIVA logo_epsA 2) \$2)) (logo_FWRT (logo_MULL logo_UL 0) (logo_DIVA logo_UA 2) \$1))) \$0))"
+../logoDrawString $resl "iter1_l" 0 "(lambda (logo_forLoopM logo_IFTY (lambda (logo_FWRT logo_epsL (logo_DIVA logo_epsA 2) \$1)) \$0))"
+../logoDrawString $resl "iter2_l" 0 "(lambda (logo_forLoopM 2 (lambda (logo_forLoopM logo_IFTY (lambda (logo_FWRT logo_epsL (logo_DIVA logo_epsA 2) \$2)) (logo_FWRT (logo_MULL logo_UL 0) (logo_DIVA logo_UA 2) \$1))) \$0))"
+
+for i in "5" "7"; do
+  ../logoDrawString $resl "flower_${i}_h" 0 "(lambda (logo_forLoopM $i (lambda (logo_forLoopM 2 (lambda (logo_forLoopM logo_IFTY (lambda (logo_FWRT logo_epsL (logo_DIVA logo_epsA 2) \$3)) (logo_FWRT (logo_MULL logo_UL 0) (logo_DIVA logo_UA 2) \$2))) (logo_FWRT (logo_MULL logo_UL 0) (logo_DIVA (logo_MULA logo_UA 2) $i) \$1))) \$0))"
+  ../logoDrawString $resl "flower_${i}_l" 0 "(lambda (logo_forLoopM $i (lambda (logo_forLoopM 2 (lambda (logo_forLoopM logo_IFTY (lambda (logo_FWRT logo_epsL (logo_DIVA logo_epsA 2) \$3)) (logo_FWRT (logo_MULL logo_UL 0) (logo_DIVA logo_UA 2) \$2))) (logo_FWRT (logo_MULL logo_UL 0) (logo_DIVA (logo_MULA logo_UA 2) $i) \$1))) \$0))"
+done
+
+
