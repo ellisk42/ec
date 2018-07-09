9ad6398172209cae16a23629c7db451ae139d5a6
Kevin Ellis <ellisk@mit.edu>
2018-07-08 21:10:35 -0400

    switch compressor to pypy - rust is dying mysteriously

--- logo.py
+++ logo.py
@@ -178,7 +178,7 @@ if __name__ == "__main__":
     generator = ecIterator(baseGrammar, train,
                            testingTasks=test,
                            outputPrefix=prefix_pickles,
-                           compressor="rust",
+                           compressor="pypy",
                            evaluationTimeout=0.01,
                            **args)
 
