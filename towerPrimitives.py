2bf153f918ceb9100bf2ef2162605430f5b3b80a
Kevin Ellis <ellisk@mit.edu>
2018-08-16 10:16:12 -0400

    improved supervised tower tasks

--- towerPrimitives.py
+++ towerPrimitives.py
@@ -79,7 +79,7 @@ primitives = [
      for name, (w, h) in blocks.items()] + [
              addition, subtraction
          ] + \
-         [Primitive(str(j), tint, j) for j in list(range(1,3)) + list(range(-2,0,-1)) ]
+         [Primitive(str(j), tint, j) for j in list(range(1,6)) + list(range(-2,0,-1)) ]
 
 def executeTower(p):
     return p.evaluate([])(lambda s: (s,[]))(0)[1]
