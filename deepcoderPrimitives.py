080bdc46ebb6f17350ebd25611738779ce9af850
Maxwell Nye <mnye@mit.edu>
2018-08-16 12:39:44 -0400

    modified deepcoderPrimitives.py

--- deepcoderPrimitives.py
+++ deepcoderPrimitives.py
@@ -1,9 +1,10 @@
-from program import Primitive, Program
+from program import Primitive, Program, prettyProgram
 from grammar import Grammar
-from type import tlist, tint, tbool, arrow, t0, t1, t2
+from type import tlist, tint, tbool, arrow #, t0, t1, t2
+
 
 import math
-from functools import reduce
+#from functools import reduce
 
 #deepcoderPrimitives
 Null = None #or perhaps something else, like "an integer outside the working range"?
@@ -77,181 +78,182 @@ def deepcoderPrimitives():
         Primitive("zipwith", arrow(arrow(tint, tint, tint), tlist(tint), tlist(tint), tlist(tint)), _zipwith), #is this okay???
         Primitive("scanl1", arrow(arrow(tint, tint, tint), tlist(tint), tlist(tint)), _scanl1), #is this okay???
         ] + [
-        Primitive("succ", arrow(tint, tint), _succ)
-        Primitive("pred", arrow(tint, tint), _pred)
-        Primitive("double", arrow(tint, tint), _double)
-        Primitive("half", arrow(tint, tint), _half)
-        Primitive("neg", arrow(tint, tint), _neg)
-        Primitive("square", arrow(tint, tint), _square)
-        Primitive("triple", arrow(tint, tint), _triple)
-        Primitive("third", arrow(tint, tint), _third)
-        Primitive("quad", arrow(tint, tint), _quad)
-        Primitive("quarter", arrow(tint, tint), _quarter)
+        Primitive("succ", arrow(tint, tint), _succ),
+        Primitive("pred", arrow(tint, tint), _pred),
+        Primitive("double", arrow(tint, tint), _double),
+        Primitive("half", arrow(tint, tint), _half),
+        Primitive("neg", arrow(tint, tint), _neg),
+        Primitive("square", arrow(tint, tint), _square),
+        Primitive("triple", arrow(tint, tint), _triple),
+        Primitive("third", arrow(tint, tint), _third),
+        Primitive("quad", arrow(tint, tint), _quad),
+        Primitive("quarter", arrow(tint, tint), _quarter),
         ] + [
-        Primitive("pos", arrow(tint, tbool), _pos)
-        Primitive("neg", arrow(tint, tbool), _neg)
-        Primitive("even", arrow(tint, tbool), _even)
-        Primitive("odd", arrow(tint, tbool), _odd)
+        Primitive("pos", arrow(tint, tbool), _pos),
+        Primitive("neg", arrow(tint, tbool), _neg),
+        Primitive("even", arrow(tint, tbool), _even),
+        Primitive("odd", arrow(tint, tbool), _odd),
         ] + [
-        Primitive("add", arrow(tint, tint, tint), _add)
-        Primitive("sub", arrow(tint, tint, tint), _sub)
-        Primitive("mult", arrow(tint, tint, tint), _mult)
-        Primitive("min", arrow(tint, tint, tint), _min)
+        Primitive("add", arrow(tint, tint, tint), _add),
+        Primitive("sub", arrow(tint, tint, tint), _sub),
+        Primitive("mult", arrow(tint, tint, tint), _mult),
+        Primitive("min", arrow(tint, tint, tint), _min),
         Primitive("max", arrow(tint, tint, tint), _max)
     ]
 
 
 def deepcoderProductions():
-    #TODO
-    pass
+    return [(0.0, prim) for prim in deepcoderPrimitives()]
 
 
 if __name__ == "__main__":
-    bootstrapTarget()
-    import pickle
-    g = Grammar.uniform(McCarthyPrimitives())
-    # with open("/home/ellisk/om/ec/experimentOutputs/list_aic=1.0_arity=3_ET=1800_expandFrontier=2.0_it=4_likelihoodModel=all-or-nothing_MF=5_baseline=False_pc=10.0_L=1.0_K=5_rec=False.pickle", "rb") as handle:
-    #     b = pickle.load(handle).grammars[-1]
-    # print b
+    #g = Grammar.uniform(deepcoderPrimitives())
+    g = Grammar.fromProductions(deepcoderProductions(), logVariable= -100000000000)
+    p = g.sample(arrow(tlist(tint), tint))
+    print(prettyProgram(p))
 
-    p = Program.parse(
-        "(lambda (lambda (lambda (if (empty? $0) empty (cons (+ (car $1) (car $0)) ($2 (cdr $1) (cdr $0)))))))")
-    t = arrow(tlist(tint), tlist(tint), tlist(tint))  # ,tlist(tbool))
-    print(g.logLikelihood(arrow(t, t), p))
-    assert False
-    print(b.logLikelihood(arrow(t, t), p))
+    # # with open("/home/ellisk/om/ec/experimentOutputs/list_aic=1.0_arity=3_ET=1800_expandFrontier=2.0_it=4_likelihoodModel=all-or-nothing_MF=5_baseline=False_pc=10.0_L=1.0_K=5_rec=False.pickle", "rb") as handle:
+    # #     b = pickle.load(handle).grammars[-1]
+    # # print b
 
+    # p = Program.parse(
+    #     "(lambda (lambda (lambda (if (empty? $0) empty (cons (+ (car $1) (car $0)) ($2 (cdr $1) (cdr $0)))))))")
+    # t = arrow(tlist(tint), tlist(tint), tlist(tint))  # ,tlist(tbool))
+    # print(g.logLikelihood(arrow(t, t), p))
+    # assert False
+    # print(b.logLikelihood(arrow(t, t), p))
+
+    # # p = Program.parse("""(lambda (lambda
+    # # (unfold 0
+    # # (lambda (+ (index $0 $2) (index $0 $1)))
+    # # (lambda (1+ $0))
+    # # (lambda (eq? $0 (length $1))))))
+    # # """)
     # p = Program.parse("""(lambda (lambda
-    # (unfold 0
-    # (lambda (+ (index $0 $2) (index $0 $1)))
-    # (lambda (1+ $0))
-    # (lambda (eq? $0 (length $1))))))
-    # """)
-    p = Program.parse("""(lambda (lambda
-    (map (lambda (+ (index $0 $2) (index $0 $1))) (range (length $0))  )))""")
-    # .replace("unfold", "#(lambda (lambda (lambda (lambda (fix1 $0 (lambda (lambda (#(lambda (lambda (lambda (if $0 empty (cons $1 $2))))) ($1 ($3 $0)) ($4 $0) ($5 $0)))))))))").\
-    # replace("length", "#(lambda (fix1 $0 (lambda (lambda (if (empty? $0) 0 (+ ($1 (cdr $0)) 1))))))").\
-    # replace("forloop", "(#(lambda (lambda (lambda (lambda (fix1 $0 (lambda (lambda (#(lambda (lambda (lambda (if $0 empty (cons $1 $2))))) ($1 ($3 $0)) ($4 $0) ($5 $0))))))))) (lambda (#(eq? 0) $0)) $0 (lambda (#(lambda (- $0 1)) $0)))").\
-    # replace("inc","#(lambda (+ $0 1))").\
-    # replace("drop","#(lambda (lambda (fix2 $0 $1 (lambda (lambda (lambda (if
-    # (#(eq? 0) $1) $0 (cdr ($2 (- $1 1) $0)))))))))"))
-    print(p)
-    print(g.logLikelihood(t, p))
-    assert False
+    # (map (lambda (+ (index $0 $2) (index $0 $1))) (range (length $0))  )))""")
+    # # .replace("unfold", "#(lambda (lambda (lambda (lambda (fix1 $0 (lambda (lambda (#(lambda (lambda (lambda (if $0 empty (cons $1 $2))))) ($1 ($3 $0)) ($4 $0) ($5 $0)))))))))").\
+    # # replace("length", "#(lambda (fix1 $0 (lambda (lambda (if (empty? $0) 0 (+ ($1 (cdr $0)) 1))))))").\
+    # # replace("forloop", "(#(lambda (lambda (lambda (lambda (fix1 $0 (lambda (lambda (#(lambda (lambda (lambda (if $0 empty (cons $1 $2))))) ($1 ($3 $0)) ($4 $0) ($5 $0))))))))) (lambda (#(eq? 0) $0)) $0 (lambda (#(lambda (- $0 1)) $0)))").\
+    # # replace("inc","#(lambda (+ $0 1))").\
+    # # replace("drop","#(lambda (lambda (fix2 $0 $1 (lambda (lambda (lambda (if
+    # # (#(eq? 0) $1) $0 (cdr ($2 (- $1 1) $0)))))))))"))
+    # print(p)
+    # print(g.logLikelihood(t, p))
+    # assert False
 
-    print("??")
-    p = Program.parse(
-        "#(lambda (#(lambda (lambda (lambda (fix1 $0 (lambda (lambda (if (empty? $0) $3 ($4 (car $0) ($1 (cdr $0)))))))))) (lambda $1) 1))")
-    for j in range(10):
-        l = list(range(j))
-        print(l, p.evaluate([])(lambda x: x * 2)(l))
-        print()
-    print()
+    # print("??")
+    # p = Program.parse(
+    #     "#(lambda (#(lambda (lambda (lambda (fix1 $0 (lambda (lambda (if (empty? $0) $3 ($4 (car $0) ($1 (cdr $0)))))))))) (lambda $1) 1))")
+    # for j in range(10):
+    #     l = list(range(j))
+    #     print(l, p.evaluate([])(lambda x: x * 2)(l))
+    #     print()
+    # print()
 
-    print("multiply")
-    p = Program.parse(
-        "(lambda (lambda (lambda (if (eq? $0 0) 0 (+ $1 ($2 $1 (- $0 1)))))))")
-    print(g.logLikelihood(arrow(arrow(tint, tint, tint), tint, tint, tint), p))
-    print()
+    # print("multiply")
+    # p = Program.parse(
+    #     "(lambda (lambda (lambda (if (eq? $0 0) 0 (+ $1 ($2 $1 (- $0 1)))))))")
+    # print(g.logLikelihood(arrow(arrow(tint, tint, tint), tint, tint, tint), p))
+    # print()
 
-    print("take until 0")
-    p = Program.parse("(lambda (lambda (if (eq? $1 0) empty (cons $1 $0))))")
-    print(g.logLikelihood(arrow(tint, tlist(tint), tlist(tint)), p))
-    print()
+    # print("take until 0")
+    # p = Program.parse("(lambda (lambda (if (eq? $1 0) empty (cons $1 $0))))")
+    # print(g.logLikelihood(arrow(tint, tlist(tint), tlist(tint)), p))
+    # print()
 
-    print("countdown primitive")
-    p = Program.parse(
-        "(lambda (lambda (if (eq? $0 0) empty (cons (+ $0 1) ($1 (- $0 1))))))")
-    print(
-        g.logLikelihood(
-            arrow(
-                arrow(
-                    tint, tlist(tint)), arrow(
-                    tint, tlist(tint))), p))
-    print(_fix(9)(p.evaluate([])))
-    print("countdown w/ better primitives")
-    p = Program.parse(
-        "(lambda (lambda (if (eq0 $0) empty (cons (+1 $0) ($1 (-1 $0))))))")
-    print(
-        g.logLikelihood(
-            arrow(
-                arrow(
-                    tint, tlist(tint)), arrow(
-                    tint, tlist(tint))), p))
+    # print("countdown primitive")
+    # p = Program.parse(
+    #     "(lambda (lambda (if (eq? $0 0) empty (cons (+ $0 1) ($1 (- $0 1))))))")
+    # print(
+    #     g.logLikelihood(
+    #         arrow(
+    #             arrow(
+    #                 tint, tlist(tint)), arrow(
+    #                 tint, tlist(tint))), p))
+    # print(_fix(9)(p.evaluate([])))
+    # print("countdown w/ better primitives")
+    # p = Program.parse(
+    #     "(lambda (lambda (if (eq0 $0) empty (cons (+1 $0) ($1 (-1 $0))))))")
+    # print(
+    #     g.logLikelihood(
+    #         arrow(
+    #             arrow(
+    #                 tint, tlist(tint)), arrow(
+    #                 tint, tlist(tint))), p))
 
-    print()
+    # print()
 
-    print("prepend zeros")
-    p = Program.parse(
-        "(lambda (lambda (lambda (if (eq? $1 0) $0 (cons 0 ($2 (- $1 1) $0))))))")
-    print(
-        g.logLikelihood(
-            arrow(
-                arrow(
-                    tint,
-                    tlist(tint),
-                    tlist(tint)),
-                tint,
-                tlist(tint),
-                tlist(tint)),
-            p))
-    print()
-    assert False
+    # print("prepend zeros")
+    # p = Program.parse(
+    #     "(lambda (lambda (lambda (if (eq? $1 0) $0 (cons 0 ($2 (- $1 1) $0))))))")
+    # print(
+    #     g.logLikelihood(
+    #         arrow(
+    #             arrow(
+    #                 tint,
+    #                 tlist(tint),
+    #                 tlist(tint)),
+    #             tint,
+    #             tlist(tint),
+    #             tlist(tint)),
+    #         p))
+    # print()
+    # assert False
 
-    p = Program.parse(
-        "(lambda (fix1 $0 (lambda (lambda (if (empty? $0) 0 (+ 1 ($1 (cdr $0))))))))")
-    print(p.evaluate([])(list(range(17))))
-    print(g.logLikelihood(arrow(tlist(tbool), tint), p))
+    # p = Program.parse(
+    #     "(lambda (fix1 $0 (lambda (lambda (if (empty? $0) 0 (+ 1 ($1 (cdr $0))))))))")
+    # print(p.evaluate([])(list(range(17))))
+    # print(g.logLikelihood(arrow(tlist(tbool), tint), p))
 
-    p = Program.parse(
-        "(lambda (lambda (if (empty? $0) 0 (+ 1 ($1 (cdr $0))))))")
-    print(
-        g.logLikelihood(
-            arrow(
-                arrow(
-                    tlist(tbool), tint), arrow(
-                    tlist(tbool), tint)), p))
+    # p = Program.parse(
+    #     "(lambda (lambda (if (empty? $0) 0 (+ 1 ($1 (cdr $0))))))")
+    # print(
+    #     g.logLikelihood(
+    #         arrow(
+    #             arrow(
+    #                 tlist(tbool), tint), arrow(
+    #                 tlist(tbool), tint)), p))
 
-    p = Program.parse(
-        "(lambda (fix1 $0 (lambda (lambda (if (empty? $0) 0 (+ (car $0) ($1 (cdr $0))))))))")
+    # p = Program.parse(
+    #     "(lambda (fix1 $0 (lambda (lambda (if (empty? $0) 0 (+ (car $0) ($1 (cdr $0))))))))")
 
-    print(p.evaluate([])(list(range(4))))
-    print(g.logLikelihood(arrow(tlist(tint), tint), p))
+    # print(p.evaluate([])(list(range(4))))
+    # print(g.logLikelihood(arrow(tlist(tint), tint), p))
 
-    p = Program.parse(
-        "(lambda (lambda (if (empty? $0) 0 (+ (car $0) ($1 (cdr $0))))))")
-    print(p)
-    print(
-        g.logLikelihood(
-            arrow(
-                arrow(
-                    tlist(tint),
-                    tint),
-                tlist(tint),
-                tint),
-            p))
+    # p = Program.parse(
+    #     "(lambda (lambda (if (empty? $0) 0 (+ (car $0) ($1 (cdr $0))))))")
+    # print(p)
+    # print(
+    #     g.logLikelihood(
+    #         arrow(
+    #             arrow(
+    #                 tlist(tint),
+    #                 tint),
+    #             tlist(tint),
+    #             tint),
+    #         p))
 
-    print("take")
-    p = Program.parse(
-        "(lambda (lambda (lambda (if (eq? $1 0) empty (cons (car $0) ($2 (- $1 1) (cdr $0)))))))")
-    print(p)
-    print(
-        g.logLikelihood(
-            arrow(
-                arrow(
-                    tint,
-                    tlist(tint),
-                    tlist(tint)),
-                tint,
-                tlist(tint),
-                tlist(tint)),
-            p))
-    assert False
+    # print("take")
+    # p = Program.parse(
+    #     "(lambda (lambda (lambda (if (eq? $1 0) empty (cons (car $0) ($2 (- $1 1) (cdr $0)))))))")
+    # print(p)
+    # print(
+    #     g.logLikelihood(
+    #         arrow(
+    #             arrow(
+    #                 tint,
+    #                 tlist(tint),
+    #                 tlist(tint)),
+    #             tint,
+    #             tlist(tint),
+    #             tlist(tint)),
+    #         p))
+    # assert False
 
-    print(p.evaluate([])(list(range(4))))
-    print(g.logLikelihood(arrow(tlist(tint), tlist(tint)), p))
+    # print(p.evaluate([])(list(range(4))))
+    # print(g.logLikelihood(arrow(tlist(tint), tlist(tint)), p))
 
-    p = Program.parse(
-        """(lambda (fix (lambda (lambda (match $0 0 (lambda (lambda (+ $1 ($3 $0))))))) $0))""")
-    print(p.evaluate([])(list(range(4))))
-    print(g.logLikelihood(arrow(tlist(tint), tint), p))
+    # p = Program.parse(
+    #     """(lambda (fix (lambda (lambda (match $0 0 (lambda (lambda (+ $1 ($3 $0))))))) $0))""")
+    # print(p.evaluate([])(list(range(4))))
+    # print(g.logLikelihood(arrow(tlist(tint), tint), p))
