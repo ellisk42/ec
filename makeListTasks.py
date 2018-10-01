5fcf9a4e0321702598255ed7631ad402f1b841e4
Kevin Ellis <ellisk@mit.edu>
2018-10-01 11:14:32 -0400

    stuttering tasks

--- makeListTasks.py
+++ makeListTasks.py
@@ -130,54 +130,6 @@ def make_list_bootstrap_tasks():
               for l in [randomList()]]),
     ]
 
-    # Encourages learning of simple nonrecursive operations that are useful in
-    # recursive functions
-    decrementBootstrap = [
-        Task("decrement", arrow(tint, tint),
-             [((n,), n - 1) for n in range(10)]),
-        Task("decrement twice", arrow(tint, tint),
-             [((n,), n - 2) for n in range(10)]),
-        Task("decrement car", arrow(tlist(tint), tint),
-             [((l,), l[0] - 1)
-              for _ in range(10)
-              for l in [randomList()]]),
-    ]
-    incrementBootstrap = [
-        Task("increment", arrow(tint, tint),
-             [((n,), n + 1) for n in range(10)]),
-        Task("increment car", arrow(tlist(tint), tint),
-             [((l,), l[0] + 1)
-              for _ in range(10)
-              for l in [randomList()]]),
-        Task("increment twice", arrow(tint, tint),
-             [((n,), n + 2) for n in range(10)]),
-    ]
-    operationBootstrap = decrementBootstrap + incrementBootstrap + [
-        Task("zero?", arrow(tint, tbool),
-             [((n,), n == 0) for n in range(10)]),
-        Task("zero car?", arrow(tlist(tint), tbool),
-             [(([h] + l,), h == 0)
-              for _ in range(5)
-              for h in [0, randint(1, 9)]
-              for l in [randomList()]]),
-        Task("second argument zero?", arrow(tint, tint, tbool),
-             [((a, b), b == 0)
-              for _ in range(5)
-              for b in [0, randint(1, 9)]
-              for a in [randint(1, 9)]]),
-        Task("or", arrow(tbool, tbool, tbool),
-             [((a, b), a or b)
-              for a in [True, False]
-              for b in [True, False]]),
-        Task("and", arrow(tbool, tbool, tbool),
-             [((a, b), a and b)
-              for a in [True, False]
-              for b in [True, False]]),
-        Task("singleton", arrow(tint, tlist(tint)),
-             [((n,), [n])
-              for n in range(10)]),
-    ]
-
     # Encourages learning of unfolding
     unfoldBootstrap = [
         Task("countdown", arrow(tint, tlist(tint)),
@@ -190,6 +142,14 @@ def make_list_bootstrap_tasks():
              [((l,), [x for j,x in enumerate(l) if j%2 == 0])
               for _ in range(10)
               for l in [ [randint(0, 9) for _ in range(randint(1,4)*2)] ] ]),
+        Task("stutter every other", arrow(tlist(tint),tlist(tint)),
+             [((l,), [l[int(j/2)] for j in range(len(l)) ])
+              for _ in range(10)
+              for l in [ [randint(0, 9) for _ in range(randint(1,4)*2)] ] ]),
+        Task("stutter", arrow(tlist(tint),tlist(tint)),
+             [((l,), [z for x in l for z in [x,x] ])
+              for _ in range(10)
+              for l in [randomList()] ]),
         Task("drop last element", arrow(tlist(tint),tlist(tint)),
              [((l,), l[:-1])
               for _ in range(10)
