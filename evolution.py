2407e740bb16af6c476e3b9b6fd6e86eda7ad46a
Kevin Ellis <ellisk@mit.edu>
2018-10-18 19:55:45 -0400

    random evolution coding

--- evolution.py
+++ evolution.py
@@ -30,14 +30,100 @@ class EvolutionGuide(RecognitionModel):
     def mutationAndFitness(self, goal, current):
         features = self._MLP(self.featureExtractor.featuresOfTask(goal, current))
         return self.policy(features), self.value(features)
+
+    def children(self, goal, _=None,
+                 ancestor=None, timeout=None):
+        g = self.mutationGrammar(goal, ancestor).untorch()
+        message = {"DSL": g.json(),
+                   "request": goal.request.json(),
+                   "extras": [[]],
+                   "timeout": float(timeout)
+        }
+        if ancestor is not None: message["ancestor"] = str(ancestor)
+
+        response = jsonBinaryInvoke("./evolution", message)
+        children = []
+        for e in response:
+            mutation = Program.parse(e['programs'][0])
+            if ancestor is None: child = mutation
+            else: child = Application(mutation,ancestor)
+            children.append(child)
+        return children
+
+    def search(self, goal, _=None,
+               populationSize=None, timeout=None, generations=None):
+        assert populationSize is not None
+        assert timeout is not None
+        assert generations is not None
+
+        # Map from parent to fitness
+        population = {None: 1.}
+        everyChild = set()
+
+        for _ in range(generations):
+            z = sum(population.values())
+            children = []
+            for ancestor, fitness in population.items():
+                children.extend(self.children(goal, ancestor,
+                                              timeout=timeout*fitness/z))
+            population = {child: self.getFitness(goal, self.taskOfProgram(child)).view(-1).data[0]
+                          for child in set(children) }
+            for child in population: everyChild.add(child)
+
+        return everyChild
+            
+                
+def possibleAncestors(request, program):
+    from itertools import permutations
+
+    eprint(program.infer())
     
+    desiredNumberOfArguments = len(request.functionArguments())
+    def curse(d, p):
+        # Returns a set of (mutation, ancestor)
+        parses = set()
+
+        # Could this be the ancestor?
+        freeVariableTypes = {}
+        context, tp = p.inferType(Context.EMPTY, [], freeVariableTypes)
+        if len(freeVariableTypes) + len(tp.functionArguments()) == desiredNumberOfArguments:
+            for fv in permutations( (fi, ft.apply(context))
+                                    for fi, ft in freeVariableTypes.items()):
+                t = tp
+                for _,fvt in reversed(fv): t = arrow(fvt,t)
+                if canUnify(t, request):
+                    parses.add((Index(d), p))
+
+        if p.isIndex:
+            parses.add((Index(p.i + 1),None))
+        if p.isPrimitive:
+            parses.add((p,None))
+        if p.isApplication:
+            f = curse(d, p.f)
+            x = curse(d, p.x)
+            for fp,fa in f:
+                for xp,xa in x:
+                    if fa is not None and \
+                       xa is not None and \
+                       fa != xa:
+                        continue
+                    a = fa or xa
+                    parses.add((Application(fp,xp), a))
+        if p.isAbstraction:
+            for b,a in curse(d + 1, p.body):
+                parses.add((Abstraction(b), a))
+        return parses
+
+    return curse(0, program)
+
 
 bootstrapTarget()
 g = Grammar.uniform([Program.parse(p)
-                     for p in ["+","-","0","1",
+                     for p in ["+","-","0","1","car",
                                "fold","empty","cons"] ])
 
-
+eprint(possibleAncestors(arrow(tlist(tint),tlist(tint)),
+                         Program.parse("(lambda (fold $0 (cons (car $0) empty) (lambda (lambda (cons $1 $0)))))")))
 def children(g, request, _=None,
              ancestor=None, timeout=None):
     message = {"DSL": g.json(),
