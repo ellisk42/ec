55e877e3137c2bf82a01c688d83e07c0d61539b0
Cathy Wong <wong.catherine.mail@gmail.com>
2019-01-11 15:17:25 -0500

    Fix

--- taskRankGraphs.py
+++ taskRankGraphs.py
@@ -332,7 +332,7 @@ def plotTSNE(
 
 			if labelWithImages:
 				images = []
-				for i, task in enumerate(recognitionTaskMetrics):
+				for i, task in enumerate(sorted(recognitionTaskMetrics.keys(), key=lambda task : task.name)): # Enumerate in same order as sorted tasks.
 					if 'taskImages' not in recognitionTaskMetrics[task] and domain == 'tower': recognitionTaskMetrics[task]['taskImages'] = task.getImage(pretty=True) # BUG: this shouldn't be necessaryd
 					if 'taskImages' not in recognitionTaskMetrics[task] and domain == 'rational': recognitionTaskMetrics[task]['taskImages'] = task.features
 					if 'taskImages' not in recognitionTaskMetrics[task] and domain == 'logo': recognitionTaskMetrics[task]['taskImages'] = task.highresolution
