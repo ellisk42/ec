dd5acb26d43fe2a08e9003d4d0384cc163fe00ae
Cathy Wong <wong.catherine.mail@gmail.com>
2019-01-11 15:14:28 -0500

    Fix bug introduced by sorting

--- taskRankGraphs.py
+++ taskRankGraphs.py
@@ -259,6 +259,20 @@ def makeTowerImage(im):
 	im = np.dstack([im, alpha])
 	return im
 
+def makeRationalImage(im):
+	im = np.reshape(np.array(im),(64, 64))
+	# Make black and white.
+	black_mask = im != 0
+	white_mask = im == 0
+	im[black_mask] = 0
+	im[white_mask] = 255
+
+	alpha = np.zeros(im.shape)
+	alpha[black_mask] = 255
+	# Need to make it the opposite for alpha.
+	im = np.dstack([im, im, im, alpha])
+	return im
+
 def printTaskExamples(taskType, task):
 	print(task.name)
 	for example in task.examples:
@@ -318,14 +332,17 @@ def plotTSNE(
 
 			if labelWithImages:
 				images = []
-				# Need to go over in the same order as before.
-				for i, task in enumerate(sorted(recognitionTaskMetrics.keys(), key=lambda task : task.name)):
-					if 'taskImages' not in recognitionTaskMetrics[task] and domain == 'tower': recognitionTaskMetrics[task]['taskImages'] = task.getImage(pretty=True) # BUG: this shouldn't be necessary
+				for i, task in enumerate(recognitionTaskMetrics):
+					if 'taskImages' not in recognitionTaskMetrics[task] and domain == 'tower': recognitionTaskMetrics[task]['taskImages'] = task.getImage(pretty=True) # BUG: this shouldn't be necessaryd
+					if 'taskImages' not in recognitionTaskMetrics[task] and domain == 'rational': recognitionTaskMetrics[task]['taskImages'] = task.features
+					if 'taskImages' not in recognitionTaskMetrics[task] and domain == 'logo': recognitionTaskMetrics[task]['taskImages'] = task.highresolution
 					im = np.array(recognitionTaskMetrics[task]['taskImages'])
 					if domain == 'logo':
 						im = makeLogoImage(im)
 					elif domain == 'tower':
 						im = makeTowerImage(im)
+					elif domain == 'rational':
+						im = makeRationalImage(im)
 					images.append(im)
 
 				plotEmbeddingWithImages(clusteredTaskMetrics, 
