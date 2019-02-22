4c986d5737b26bd2805a9d08f2d2c71f2ae6e372
Cathy Wong <wong.catherine.mail@gmail.com>
2019-02-21 19:51:01 -0500

    First pass adding puddleworld tasks

--- puddleworld.py
+++ puddleworld.py
@@ -4,7 +4,8 @@ Tasks are (gridworld, text instruction) -> goal coordinate.
 Credit: https://github.com/JannerM/spatial-reasoning 
 """
 from ec import explorationCompression, commandlineArguments, Task, ecIterator
-from makeTextTasks import makeLocalTasks, makeGlobalTasks
+from makePuddleworldTasks import makeLocalTasks, makeGlobalTasks
+from utilities import eprint, numberOfCPUs
 
 def puddleworld_options(parser):
 	parser.add_argument(
@@ -21,24 +22,18 @@ def puddleworld_options(parser):
 		)
 
 if __name__ == "__main__":
-	arguments = commandlineArguments(
-        recognitionTimeout=7200,
-        iterations=10,
-        helmholtzRatio=0.5,
-        topK=2,
-        maximumFrontier=5,
-        structurePenalty=10.,
-        a=3,
-        activation="tanh",
-        CPUs=numberOfCPUs(),
-        featureExtractor=None, #TODO(cathywong)
-        pseudoCounts=30.0,
-        extras=text_options)
+	args = commandlineArguments(
+		enumerationTimeout=10, activation='tanh', iterations=10, recognitionTimeout=3600,
+		a=3, maximumFrontier=10, topK=2, pseudoCounts=30.0,
+		helmholtzRatio=0.5, structurePenalty=1.,
+		CPUs=numberOfCPUs(),
+		extras=puddleworld_options)
 
-		doLocal, doGlobal = arguments.pop('local'), arguments.pop('global')
-		eprint("Using local tasks: %r, Using global tasks: %r")
+	doLocal, doGlobal = args.pop('local'), args.pop('global')
+	eprint("Using local tasks: %r, Using global tasks: %r" % (doLocal, doGlobal))
 
-		localTrain, localTest = makeLocalTasks() if doLocal else []
-		globalTrain, globalTest = makeGlobalTasks() if doGlobal else []
+	localTrain, localTest = makeLocalTasks() if doLocal else []
+	globalTrain, globalTest = makeGlobalTasks() if doGlobal else []
 		
 
+	assert False
