43831c429b14e5d16ee2a76eb50508398123cc8a
Cathy Wong <wong.catherine.mail@gmail.com>
2019-03-13 16:34:42 -0400

    Steps towards adding puddleworld submodule.

--- puddleworldPrimitives.py
+++ puddleworldPrimitives.py
@@ -1,6 +1,28 @@
+"""
+Puddleworld primitives.
+
+Converts a PyCCG ontology into primitives for Dreamcoder.
+"""
+import sys
+sys.path.insert(0, "pyccg/nltk")
+from pyccg.pyccg.logic import TypeSystem, Ontology, Expression
+from puddleworld.learner import obj_dict
+
 from type import baseType
+from utilities import eprint
 
 
+"""
 tLayoutMap = baseType('layoutMap')
 tObjectMap = baseType('ObjectMap')
 tLocation= baseType('location')
+
+primitives = []
+"""
+
+def loadPuddleworldOntology():
+	return []
+
+
+if __name__ == "__main__":
+	primitives = loadPuddleworldOntology()
