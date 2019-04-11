335a3f3054175da331325aef8f0962b021e79ca8
Cathy Wong <wong.catherine.mail@gmail.com>
2019-04-11 16:17:51 -0400

    Remove puddleworld subdir

--- puddleworldPrimitives.py
+++ puddleworldPrimitives.py
@@ -4,9 +4,10 @@ Puddleworld primitives.
 Converts a PyCCG ontology into primitives for Dreamcoder.
 """
 import sys
+sys.path.insert(0, "pyccg")
 sys.path.insert(0, "pyccg/nltk")
-from pyccg.pyccg.logic import TypeSystem, Ontology, Expression
-from puddleworld.learner import obj_dict
+from pyccg.logic import TypeSystem, Ontology, Expression
+from puddleworld.ontology import puddleworldOntology
 
 from type import baseType
 from utilities import eprint
@@ -22,7 +23,6 @@ primitives = []
 
 def loadPuddleworldOntology():
 	return []
-
 
 if __name__ == "__main__":
 	primitives = loadPuddleworldOntology()
