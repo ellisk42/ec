2bd9f476f4508025837571853ddd6603d3ae176e
lucast4 <lucasytian@gmail.com>
2019-09-12 11:28:04 -0400

    started draw domain

---
+++ dreamcoder/domains/draw/main.py
@@ -0,0 +1,19 @@
+from dreamcoder.domains.draw.makeDrawTasks import drawDrawings
+from dreamcoder.domains.draw.drawPrimitives import primitives, taxes, tartist, tangle, tscale, tdist
+
+# from dreamcoder.dreamcoder import ecIterator
+from dreamcoder.grammar import Grammar
+# from dreamcoder.program import Program
+# from dreamcoder.recognition import variable, maybe_cuda
+from dreamcoder.task import Task
+from dreamcoder.type import arrow
+# from dreamcoder.utilities import eprint, testTrainSplit, loadPickle
+
+def dreamFromGrammar(g, directory, N=50):
+	request = arrow(taxes, taxes)
+    programs = [ p
+                 for _ in range(N)
+                 for p in [g.sample(request,
+                                    maximumDepth=20)]
+                 if p is not None]
+    drawDrawings(*programs, filenames)
---
+++ dreamcoder/domains/draw/makeDrawTasks.py
@@ -0,0 +1,15 @@
+from dreamcoder.domains.draw.makeDrawTasks import drawDrawings
+from dreamcoder.domains.draw.drawPrimitives import primitives, taxes, tartist, tangle, tscale, tdist
+# from dreamcoder.dreamcoder import ecIterator
+from dreamcoder.grammar import Grammar
+from dreamcoder.program import Program
+# from dreamcoder.recognition import variable, maybe_cuda
+from dreamcoder.task import Task
+from dreamcoder.type import arrow
+# from dreamcoder.utilities import eprint, testTrainSplit, loadPickle
+
+
+def drawDrawings(programs):
+	# TODO: code to take in programs and render into image
+	# all valid programs must output type "taxes" which can render
+	pass
