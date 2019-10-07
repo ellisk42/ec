2d455a455a94c56174b51c5befcdfe981c64427a
lucast4 <lucasytian@gmail.com>
2019-10-07 16:04:50 -0400

    trainig set for S8

--- dreamcoder/domains/draw/makeDrawTasks.py
+++ dreamcoder/domains/draw/makeDrawTasks.py
@@ -15,95 +15,93 @@ import numpy as np
 
 
 class SupervisedDraw(Task):
-    def __init__(self, name, program):
-        super(SupervisedDraw, self).__init__(name, tstroke, [],
-                                              features=[]) # TODO: LT, needs this, i.e., a request.
+	def __init__(self, name, program):
+		super(SupervisedDraw, self).__init__(name, tstroke, [],
+											  features=[]) # TODO: LT, needs this, i.e., a request.
 
-        # compute the trajectory, which is a list of lines
-        trajectory = []
-        for segments in program:
-            for i in range(segments.shape[0] - 1):
-                p1 = segments[i][0], segments[i][1]
-                p2 = segments[i+1][0], segments[i+1][1]
-                trajectory.append([p1,p2])
-        self.specialTask = ("draw",
-                            {"trajectory": trajectory})
+		# compute the trajectory, which is a list of lines
+		trajectory = []
+		for segments in program:
+			for i in range(segments.shape[0] - 1):
+				p1 = segments[i][0], segments[i][1]
+				p2 = segments[i+1][0], segments[i+1][1]
+				trajectory.append([p1,p2])
+		self.specialTask = ("draw",
+							{"trajectory": trajectory})
 
-        self.strokes = program # list of np arrays.
-        self.rendered_strokes = prog2pxl(program)
+		self.strokes = program # list of np arrays.
+		self.rendered_strokes = prog2pxl(program)
 
-    def logLikelihood(self, e, timeout=None): # TODO, LT, given expression, calculates distance.
-        # from dreamcoder.domains.tower.tower_common import centerTower
-        if False:
-            p1 = self.rendered_strokes
-            p2 = fig2pixel(e.evaluate([]))
+	def logLikelihood(self, e, timeout=None): # TODO, LT, given expression, calculates distance.
+		# from dreamcoder.domains.tower.tower_common import centerTower
+		if False:
+			p1 = self.rendered_strokes
+			p2 = fig2pixel(e.evaluate([]))
 
-            # l = loss(p1, p2, smoothing=2) 
-            l = loss_pxl(p1, p2)
+			# l = loss(p1, p2, smoothing=2) 
+			l = loss_pxl(p1, p2)
 
-            if l>0.1:
-                return NEGATIVEINFINITY
-            else:
-                return 0.0
-        else:
-            # print("doing it!")
-            # p1 = 
-            # p2 = prog2pxl(e.evaluate([]))
+			if l>0.1:
+				return NEGATIVEINFINITY
+			else:
+				return 0.0
+		else:
+			# print("doing it!")
+			# p1 = 
+			# p2 = prog2pxl(e.evaluate([]))
 
-            if not hasattr(e, "rendering"):
-                e.rendering = prog2pxl(e.evaluate([]))
+			if not hasattr(e, "rendering"):
+				e.rendering = prog2pxl(e.evaluate([]))
 
-            # l = loss(p1, p2, smoothing=2) 
-            l = loss_pxl(self.rendered_strokes, e.rendering)
+			# l = loss(p1, p2, smoothing=2) 
+			l = loss_pxl(self.rendered_strokes, e.rendering)
 
-            if l>0.1:
-                return NEGATIVEINFINITY
-            else:
-                return 0.0
+			if l>0.1:
+				return NEGATIVEINFINITY
+			else:
+				return 0.0
 
-    
+	
 def makeSupervisedTasks(): # TODO, LT, make these tasks.
-    # arches = [SupervisedTower("arch leg %d"%n,
-    #                           "((for i %d v) (r 4) (for i %d v) (l 2) h)"%(n,n))
-    #           for n in range(1,9)
-    # ]
-                     
-    # everything = arches + simpleLoops + Bridges + archesStacks + aqueducts + offsetArches + pyramids + bricks + staircase2 + staircase1 + compositions
-    alltasks = []
+	# arches = [SupervisedTower("arch leg %d"%n,
+	#                           "((for i %d v) (r 4) (for i %d v) (l 2) h)"%(n,n))
+	#           for n in range(1,9)
+	# ]
+					 
+	# everything = arches + simpleLoops + Bridges + archesStacks + aqueducts + offsetArches + pyramids + bricks + staircase2 + staircase1 + compositions
+	alltasks = []
+	programs = []
 
-    programs = [_line + _circle,
-    _tform(_line, _makeAffine(x=1.0)),
-    _tform(_line, _makeAffine(x=1.0, theta=pi/2)),
-    _tform(_line, _makeAffine(x=1.5)),
-    _tform(_line, _makeAffine(x=0.5)),
-    _tform(_line, _makeAffine(x=2.0)),
-    _tform(_circle, _makeAffine(x=1.5)),
-    _repeat(_line, 2, _makeAffine(x=1.0)),
-    _tform(_line, _makeAffine(x=2.)) + _tform(_circle, _makeAffine(x=1.)),
-    _line + _tform(_line, _makeAffine(x=2.)) + _tform(_circle, _makeAffine(x=1.)),
-    _repeat(_line+_tform(_circle, _makeAffine(x=1.)), 3, _makeAffine(theta=math.pi/2)),
-    ]
+	if False:
+		programs = [_line + _circle,
+		_tform(_line, _makeAffine(x=1.0)),
+		_tform(_line, _makeAffine(x=1.0, theta=pi/2)),
+		_tform(_line, _makeAffine(x=1.5)),
+		_tform(_line, _makeAffine(x=0.5)),
+		_tform(_line, _makeAffine(x=2.0)),
+		_tform(_circle, _makeAffine(x=1.5)),
+		_repeat(_line, 2, _makeAffine(x=1.0)),
+		_tform(_line, _makeAffine(x=2.)) + _tform(_circle, _makeAffine(x=1.)),
+		_line + _tform(_line, _makeAffine(x=2.)) + _tform(_circle, _makeAffine(x=1.)),
+		_repeat(_line+_tform(_circle, _makeAffine(x=1.)), 3, _makeAffine(theta=math.pi/2)),
+		]
 
-    # programs = []
-
-    # -- add some programs used in behaivor
-    if False:
-        libname = "dreamcoder/domains/draw/S6"
-        with open("{}.pkl".format(libname), 'rb') as fp:
-            P = pickle.load(fp)
-        programs.extend(P[:50])
-
-    # -- add some programs used in behavior
-    if False:
-        libname = "dreamcoder/domains/draw/S8"
-        with open("{}.pkl".format(libname), 'rb') as fp:
-            P = pickle.load(fp)
-        programs.extend(P[:10])
+	# -- add some programs used in behaivor
+	if False:
+		libname = "dreamcoder/domains/draw/S6"
+		with open("{}.pkl".format(libname), 'rb') as fp:
+			P = pickle.load(fp)
+		programs.extend(P[:50])
 
 
-    for i, p in enumerate(programs):
-        name = "task{}".format(i)
-        alltasks.append(SupervisedDraw(name, p))
+	libname = "dreamcoder/domains/draw/trainprogs/S8"
+		with open("{}.pkl".format(libname), 'rb') as fp:
+			P = pickle.load(fp)
+		programs.extend(P)
 
-    return alltasks
+	for i, p in enumerate(programs):
+		name = "task{}".format(i)
+		alltasks.append(SupervisedDraw(name, p))
+
+	return alltasks
 
