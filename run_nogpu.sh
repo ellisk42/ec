852e7c8cebb9b073929eac69c3116302481f36eb
lucast4 <lucasytian@gmail.com>
2019-10-20 17:36:28 -0400

    analyses bash scripts

---
+++ run_nogpu.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+fn=jobs/"$1"_$(date +%Y-%m-%d_%H-%M-%S)
+echo "Executing:"
+echo "${@:4}"
+echo "Exporting to $fn"
+echo "Running command:"
+echo "srun --job-name="$1" --output=jobs/"$1"_$(date +%Y-%m-%d_%H-%M-%S) --ntasks=1 --mem-per-cpu=$2 --cpus-per-task $3 -p tenenbaum --gres=gpu --time=48:00:00 singularity exec --nv container.img ${@:4} \
+    &"
+srun --job-name="$1" --output=jobs_nogpu/"$1"_$(date +%Y-%m-%d_%H-%M-%S) --ntasks=1 --mem-per-cpu=$2 --cpus-per-task $3 -p tenenbaum --time=48:00:00 singularity exec --nv container.img ${@:4} \
+    &
