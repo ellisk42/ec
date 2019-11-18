b52aa73c6d94bb45a7c26895c2ceaa670155d735
Lucas Tian <lyt@openmind7.cm.cluster>
2019-11-17 20:04:40 -0500

    prasing experimestn added

--- run_parse.sh
+++ run_parse.sh
@@ -1,5 +1,8 @@
+#!/bash/bin
+module add openmind/singularity
+
 #bash run_nogpu.sh S9.2_parse 10000 1 python analysis/parse.py "S9.2"
 # bash run_nogpu.sh S8.2.2_parse 15000 1 python analysis/parse.py "S8.2.2"
 
 
-bash run_nogpu.sh S12.10.test4_parse 10000 4 python analysis/parse.py S12.10.test4
+bash run_nogpu.sh S12.10.test4_parse 15000 2 python analysis/parse.py S12.10.test4
