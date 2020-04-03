82d1c6356f4c8f14339fee5e5ef42c9cb7608a03
Maxwell Nye <maxnye@comcast.net>
2020-04-03 00:24:48 -0400

    added om-repeat to repl run

--- searchComparisonRuns.sh
+++ searchComparisonRuns.sh
@@ -6,20 +6,21 @@ time=300
 recSteps=480000 #repl is roughly 1k/hour (0.33 steps/sec)
 ncores=8
 salt=richPrimsR1
+helmRatio=1
 #resume=experimentOutputs/listBaseIT=1 #experimentOutputs/listCathyTestEnum
 #resume=experimentOutputs/listCathyTest
 resume=experimentOutputs/listRichPrimsR1
 
 cp ${resume}.pickle ${resume}Sample.pickle
 #Train:
-sbatch -e listSample${salt}.out -o listSample${salt}.out execute_gpu_new.sh python bin/list.py --recognitionTimeout 216000 --resumeTraining -r 1 --primitives rich --split 0.5 -t ${time} -RS ${recSteps} --solver 'python'  -c ${ncores} --useValue Sample -i 2 -H 512 --resume ${resume}Sample.pickle --singleRoundValueEval &
+sbatch -e listSample${salt}.out -o listSample${salt}.out execute_gpu_new.sh python bin/list.py --recognitionTimeout 216000 --resumeTraining -r ${helmRatio} --primitives rich --split 0.5 -t ${time} -RS ${recSteps} --solver 'python'  -c ${ncores} --useValue Sample -i 2 -H 512 --resume ${resume}Sample.pickle --singleRoundValueEval &
 
 cp ${resume}.pickle ${resume}REPL.pickle
 #Train:
-sbatch -e listREPL${salt}.out -o listREPL${salt}.out execute_gpu_new.sh python bin/list.py --recognitionTimeout 216000 --resumeTraining -r 1 --primitives rich --split 0.5 -t ${time} -RS ${recSteps} --solver 'python'  -c ${ncores} --useValue AbstractREPL -i 2 -H 512 --resume ${resume}REPL.pickle --singleRoundValueEval &
+om-repeat sbatch -e listREPL${salt}.out -o listREPL${salt}.out -p tenenbaum --time=3600 --mem=64G --cpus-per-task=8 --gres=gpu:QUADRORTX6000:1 python bin/list.py --recognitionTimeout 216000 --resumeTraining -r ${helmRatio} --primitives rich --split 0.5 -t ${time} -RS ${recSteps} --solver 'python'  -c ${ncores} --useValue AbstractREPL -i 2 -H 512 --resume ${resume}REPL.pickle --singleRoundValueEval)
 
 cp ${resume}.pickle ${resume}RNN.pickle
 #Train:
-sbatch -e listRNN${salt}.out -o listRNN${salt}.out execute_gpu_new.sh python bin/list.py --recognitionTimeout 216000 --resumeTraining -r 1 --primitives rich --split 0.5 -t ${time} -RS ${recSteps} --solver 'python'  -c ${ncores} --useValue RNN -i 2 -H 512 --resume ${resume}RNN.pickle --singleRoundValueEval &
+sbatch -e listRNN${salt}.out -o listRNN${salt}.out execute_gpu_new.sh python bin/list.py --recognitionTimeout 216000 --resumeTraining -r ${helmRatio} --primitives rich --split 0.5 -t ${time} -RS ${recSteps} --solver 'python'  -c ${ncores} --useValue RNN -i 2 -H 512 --resume ${resume}RNN.pickle --singleRoundValueEval &
 
 #omitted --resumeTraining for this run
