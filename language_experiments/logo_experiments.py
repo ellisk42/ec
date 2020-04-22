db99ac31d6987a3f21eeb837939656b94a71aaeb
Catherine Wong <wong.catherine.mail@gmail.com>
2020-04-22 16:45:36 -0400

    Add no helmholtz control

--- language_experiments/logo_experiments.py
+++ language_experiments/logo_experiments.py
@@ -1,4 +1,25 @@
+USING_GCLOUD = True
+USING_SINGULARITY = False
+
+def gcloud_commands(job_name):
+    gcloud_disk_command = f"gcloud compute --project 'tenenbaumlab' disks create {job_name} --size '30' --zone 'us-east1-b' --source-snapshot 'zyzzyva-logo-language' --type 'pd-standard'"
+    gcloud_launch_commmand = f"gcloud beta compute --project=tenenbaumlab instances create {job_name} --zone=us-east1-b --machine-type=n1-standard-32 --subnet=default --network-tier=PREMIUM --maintenance-policy=MIGRATE --service-account=150557817012-compute@developer.gserviceaccount.com --scopes=https://www.googleapis.com/auth/devstorage.read_only,https://www.googleapis.com/auth/logging.write,https://www.googleapis.com/auth/monitoring.write,https://www.googleapis.com/auth/servicecontrol,https://www.googleapis.com/auth/service.management.readonly,https://www.googleapis.com/auth/trace.append --disk=name={job_name},device-name={job_name},mode=rw,boot=yes,auto-delete=yes --reservation-affinity=any"
+    return f"#######\n{gcloud_disk_command}\n\n{gcloud_launch_commmand}\n\n###Now run: \nsingularity exec ../dev-container.img "
+
 singularity_base_command = "srun --job-name=logo_language_{} --output=jobs/{} --ntasks=1 --mem-per-cpu=20000 --gres=gpu --cpus-per-task 20 --time=10000:00 --qos=tenenbaum --partition=tenenbaum singularity exec -B /om2  --nv ../dev-container.img "
+
+def get_launcher_command(job, job_name):
+    if USING_SINGULARITY:
+        return singularity_base_command.format(job, job_name)
+    else:
+        job_name = f'logo-language-{job} '
+        return gcloud_commands(job_name)
+
+def append_command(job_name):
+    if USING_SINGULARITY:
+        return  " &"
+    else:
+        return f"> jobs/{job_name} 2>&1 &"
 
 experiment_commands = []
 jobs = []
@@ -18,8 +39,7 @@ for enumerationTimeout in [1800, 3600]:
     
     base_command = "python bin/logo.py "
     
-    singularity = singularity_base_command.format(job, job_name)
-    command = singularity + base_command + base_parameters + " &"
+    command = get_launcher_command(job, job_name) + base_command + base_parameters + append_command(job_name)
     if RUN_EC_BASELINES:
         experiment_commands.append(command)
     job += 1
@@ -34,8 +54,7 @@ for enumerationTimeout in [1800, 3600]:
     
     base_command = "python bin/logo.py "
     
-    singularity = singularity_base_command.format(job, job_name)
-    command = singularity + base_command + base_parameters + " &"
+    command = get_launcher_command(job, job_name) + base_command + base_parameters + append_command(job_name)
     if RUN_EC_BASELINES:
         experiment_commands.append(command)
     job += 1
@@ -50,8 +69,7 @@ for enumerationTimeout in [1800, 3600]:
     
     base_command = "python bin/logo.py "
     
-    singularity = singularity_base_command.format(job, job_name)
-    command = singularity + base_command + base_parameters + " &"
+    command = get_launcher_command(job, job_name) + base_command + base_parameters + append_command(job_name)
     if RUN_EC_BASELINES:
         experiment_commands.append(command)
     job += 1
@@ -70,8 +88,8 @@ base_command = "python bin/logo.py "
 base_parameters = f"--enumerationTimeout {enumerationTimeout} --testingTimeout {enumerationTimeout}  --iterations {num_iterations} --biasOptimal --contextual --taskBatchSize {task_batch_size} --testEvery {test_every} --no-cuda --language_encoder recurrent --taskDataset logo_unlimited_200 --languageDataset logo_unlimited_200/synthetic "
 exp_parameters = "--recognitionEpochs 100 --recognition_0 --recognition_1 language --Helmholtz 0"
 
-singularity = singularity_base_command.format(job, job_name)
-command = singularity + base_command + base_parameters + exp_parameters + " &"
+
+command = get_launcher_command(job, job_name) + base_command + base_parameters + exp_parameters + append_command(job_name)
 if RUN_LANGUAGE_SEARCH_BASELINE:
     experiment_commands.append(command)
 job += 1
@@ -79,8 +97,8 @@ job += 1
 # Separate recognition: Enumerate, recognition_0 = examples, recognition_1 = language, compression
 job_name = "logo_ec_cnn_gru_compression_et_{}".format(enumerationTimeout)
 exp_parameters = " --recognitionTimeout 3600 --recognition_0 examples --recognition_1 language --Helmholtz 0.5"
-singularity = singularity_base_command.format(job, job_name)
-command = singularity + base_command + base_parameters + exp_parameters + " &"
+
+command = get_launcher_command(job, job_name) + base_command + base_parameters + exp_parameters + append_command(job_name)
 if RUN_LANGUAGE_SEARCH_BASELINE:
     experiment_commands.append(command)
 job += 1
@@ -88,8 +106,7 @@ job += 1
 # Finetune with language: Enumerate, recognition_0 = examples, recognition_1 = examples, language, compression
 job_name = "logo_ec_cnn_gru_cnn_compression_et_{}".format(enumerationTimeout)
 exp_parameters = " --recognitionTimeout 3600 --recognition_0 examples --recognition_1 examples language --Helmholtz 0.5 --finetune_1"
-singularity = singularity_base_command.format(job, job_name)
-command = singularity + base_command + base_parameters + exp_parameters + " &"
+command = get_launcher_command(job, job_name) + base_command + base_parameters + exp_parameters + append_command(job_name)
 if RUN_LANGUAGE_SEARCH_BASELINE:
     experiment_commands.append(command)
 job += 1
@@ -97,8 +114,7 @@ job += 1
 # Label Helmholtz with nearest.
 job_name = "logo_ec_cnn_gru_cnn_nearest_compression_et_{}".format(enumerationTimeout)
 exp_parameters = " --recognitionTimeout 3600 --recognition_0 examples --recognition_1 examples language --Helmholtz 0.5 --finetune_1  --helmholtz_nearest_language 1"
-singularity = singularity_base_command.format(job, job_name)
-command = singularity + base_command + base_parameters + exp_parameters + " &"
+command = get_launcher_command(job, job_name) + base_command + base_parameters + exp_parameters + append_command(job_name)
 if RUN_LANGUAGE_SEARCH_BASELINE:
     experiment_commands.append(command)
 job += 1
@@ -116,13 +132,13 @@ for dataset in ['logo_unlimited_200', 'logo_unlimited_500', 'logo_unlimited_1000
         jobs.append(job_name)
         base_parameters = f" --enumerationTimeout {enumerationTimeout} --testingTimeout {enumerationTimeout}  --iterations {num_iterations} --biasOptimal --contextual --taskBatchSize {task_batch_size} --testEvery {test_every} --no-cuda --recognitionSteps {recognition_steps} --recognition_0 examples --Helmholtz 0.5 --skip_first_test"
         exp_parameters = f" --taskDataset {dataset} --sample_n_supervised {sample_n_supervised}"
-        singularity = singularity_base_command.format(job, job_name)
-        command = singularity + base_command + base_parameters + exp_parameters + " &"
+        
+        command = get_launcher_command(job, job_name) + base_command + base_parameters + exp_parameters + append_command(job_name)
         if RUN_EC_BASELINES_LOGO_2:
             experiment_commands.append(command)
         job +=1
 #### Generate Helmholtz generative model experiments.
-RUN_HELMHOLTZ_GENERATIVE_MODEL = True
+RUN_HELMHOLTZ_GENERATIVE_MODEL = False
 EXPS = [('logo_unlimited_200', 0, 1), ('logo_unlimited_200', 10, 1)]
 enumerationTimeout = 1800
 num_iterations = 12
@@ -137,14 +153,34 @@ for dataset in ['logo_unlimited_200', 'logo_unlimited_500', 'logo_unlimited_1000
             jobs.append(job_name)
             base_parameters = f" --enumerationTimeout {enumerationTimeout} --testingTimeout {enumerationTimeout}  --iterations {num_iterations} --biasOptimal --contextual --taskBatchSize {task_batch_size} --testEvery {test_every} --no-cuda --recognitionSteps {recognition_steps} --recognition_0 --recognition_1 examples language --Helmholtz 0.5 --synchronous_grammar --skip_first_test"
             exp_parameters = f" --taskDataset {dataset} --language_encoder recurrent --languageDataset {dataset}/synthetic --sample_n_supervised {sample_n_supervised} --moses_dir ./moses_compiled --smt_phrase_length {phrase_length}"
-            singularity = singularity_base_command.format(job, job_name)
-            command = singularity + base_command + base_parameters + exp_parameters + " &"
+        
+            command = get_launcher_command(job, job_name) + base_command + base_parameters + exp_parameters + append_command(job_name)
             if RUN_HELMHOLTZ_GENERATIVE_MODEL:
                 if (EXPS is None) or (exp in EXPS):
                     experiment_commands.append(command)
             job +=1
-            
 
+#### Control experiments with no generative Helmholtz samples.            
+RUN_NO_HELMHOLTZ_GENERATIVE_MODEL = True
+EXPS = [('logo_unlimited_200', 0)]
+enumerationTimeout = 1800
+num_iterations = 12
+task_batch_size = 40
+test_every = 3
+recognition_steps = 10000
+for dataset in ['logo_unlimited_200', 'logo_unlimited_500', 'logo_unlimited_1000']:
+    for sample_n_supervised in [0, 10]:
+        exp = (dataset, sample_n_supervised)
+        job_name = f"logo_2_ec_cnn_gru_no_ghelm_compression_et_{enumerationTimeout}_supervised_{sample_n_supervised}_{dataset}"
+        jobs.append(job_name)
+        base_parameters = f" --enumerationTimeout {enumerationTimeout} --testingTimeout {enumerationTimeout}  --iterations {num_iterations} --biasOptimal --contextual --taskBatchSize {task_batch_size} --testEvery {test_every} --no-cuda --recognitionSteps {recognition_steps} --recognition_0 --recognition_1 examples language --Helmholtz 0 --skip_first_test"
+        exp_parameters = f" --taskDataset {dataset} --language_encoder recurrent --languageDataset {dataset}/synthetic --sample_n_supervised {sample_n_supervised} --moses_dir ./moses_compiled -"
+    
+        command = get_launcher_command(job, job_name) + base_command + base_parameters + exp_parameters + append_command(job_name)
+        if RUN_NO_HELMHOLTZ_GENERATIVE_MODEL:
+            if (EXPS is None) or (exp in EXPS):
+                experiment_commands.append(command)
+        job +=1
 
 #### Outputs
 PRINT_LOG_SCRIPT = False
@@ -152,7 +188,8 @@ PRINT_JOBS = True
 if PRINT_JOBS and not PRINT_LOG_SCRIPT:
     # print the jobs.
     print('#!/bin/bash')
-    print("module add openmind/singularity")
+    if USING_SINGULARITY:
+        print("module add openmind/singularity")
     for command in experiment_commands:
         print(command + "")
         
