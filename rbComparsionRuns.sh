751bdff3851087c3682cc3263ba8254d4e82f359
Maxwell Nye <maxnye@comcast.net>
2020-05-28 19:57:31 -0400

    code for rb policy may be done

---
+++ rbComparsionRuns.sh
@@ -0,0 +1,42 @@
+#runs to do:
+
+#checkpoint=
+
+time=1
+testingTime=1200
+recSteps=4000000 #list repl is roughly 1k/hour (0.33 steps/sec)
+ncores=8
+#salt=towers
+helmRatio=1.0
+#resume=experimentOutputs/towers
+
+
+
+for num in ""
+	do
+		#symbolic
+
+		resume=experimentOutputs/rb${num}PolicyOnly
+		salt=towers${num}PolicyOnly
+		#cp ${oldResume}.pickle ${resume}.pickle
+
+
+		#Bigram
+		#cp ${resume}.pickle ${resume}Bigram.pickle
+		cmd="python bin/rb.py --searchType Astar --contextual --testingTimeout ${testingTime} --recognitionTimeout 216000 --resumeTraining -r ${helmRatio} -t ${time} -RS ${recSteps} --solver python -c ${ncores} --useValue Sample -i 2 --resume ${resume}Bigram.pickle  --singleRoundValueEval --seed 2"
+		sbatch -e rbBigram${salt}.out -o rbBigram${salt}.out execute_gpu_new.sh ${cmd}
+		#eval "${cmd}"
+
+		#REPL
+		cp ${resume}.pickle ${resume}REPL.pickle
+		cmd="python bin/rb.py --policyType RBREPL -H 128 --searchType Astar --contextual --testingTimeout ${testingTime} --recognitionTimeout 216000 --resumeTraining -r ${helmRatio} -t ${time} -RS ${recSteps} --solver python  -c ${ncores} --useValue Sample -i 2  --resume ${resume}REPL.pickle --singleRoundValueEval --seed 2"
+		sbatch -e rbREPL${salt}.out -o rbREPL${salt}.out execute_gpu_new.sh ${cmd}
+		#eval "${cmd}"
+
+		#RNN
+		#cp ${resume}.pickle ${resume}RNN.pickle
+		cmd="python bin/rb.py --policyType RNN --searchType Astar --contextual --testingTimeout ${testingTime} --recognitionTimeout 216000 --resumeTraining -r ${helmRatio} -t ${time} -RS ${recSteps} --solver python -c ${ncores} --useValue Sample -i 2 --resume ${resume}RNN.pickle  --singleRoundValueEval --seed 2"
+		sbatch -e rbRNN${salt}.out -o rbRNN${salt}.out execute_gpu_new.sh ${cmd}
+		#eval "${cmd}"
+
+	done
